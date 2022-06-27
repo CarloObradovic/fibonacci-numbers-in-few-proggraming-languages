5cd6e126308605efa2aa30d4976414ad7ae890f1
Carlo <82326405+CarloObradovic@users.noreply.github.com>
2022-06-27 20:39:49 +0200

    Add files via upload
    IDK what tf im doing with my life, send help

---
+++ fibonacci numbers.c
@@ -0,0 +1,70 @@
+//in c programming language
+//#include <stdio.h>
+//int main()
+//{
+//    int n, i, a, b, c;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    a = 0;
+//    b = 1;
+//    c = a + b;
+//    for (i = 0; i < n; i++)
+//    {
+//        printf("%d ", c);
+//        a = b;
+//        b = c;
+//        c = a + b;
+//    }
+//    return 0;
+//}
+//in c++ programming language
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int n, i, a, b, c;
+//    cout << "Enter a number: ";
+//    cin >> n;
+//    a = 0;
+//    b = 1;
+//    c = a + b;
+//    for (i = 0; i < n; i++)
+//    {
+//        cout << c << " ";
+//        a = b;
+//        b = c;
+//        c = a + b;
+//    }
+//    return 0;
+//}
+//in java programming language
+//public class Fibonacci
+//{
+//    public static void main(String[] args)
+//    {
+//        int n, i, a, b, c;
+//        System.out.println("Enter a number: ");
+//        Scanner in = new Scanner(System.in);
+//        n = in.nextInt();
+//        a = 0;
+//        b = 1;
+//        c = a + b;
+//        for (i = 0; i < n; i++)
+//        {
+//            System.out.print(c + " ");
+//            a = b;
+//            b = c;
+//            c = a + b;
+//        }
+//    }
+//}
+//in python programming language
+//import sys
+//def fibonacci(n):
+//    a, b = 0, 1
+//    for i in range(n):
+//        a, b = b, a + b
+//        print(a)
+//fibonacci(10)
+
+
